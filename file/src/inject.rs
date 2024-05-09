88ad0ef2e50a7a68fdff58155ece1016bba4b1c5
Gabriel Mendieta Hernandez <131297265+Eth3rna1@users.noreply.github.com>
2024-05-09 15:09:12 -0500

    Update inject.rs
    Fixed some quick bugs

--- file/src/inject.rs
+++ file/src/inject.rs
@@ -4,7 +4,7 @@
 use crate::tool::{self, Constructor};
 use std::collections::HashMap;
 
-fn tab_size(line : &str) -> usize {
+fn tab_size(line: &str) -> usize {
     let mut size: usize = 0;
     for character in line.chars() {
         match character {
@@ -16,7 +16,7 @@ fn tab_size(line : &str) -> usize {
     size
 }
 
-fn indent_line(indent_size : usize, line : &str) -> String {
+fn indent_line(indent_size: usize, line: &str) -> String {
     let mut interface = " ".repeat(indent_size);
     interface += line;
     interface
@@ -31,35 +31,48 @@ impl Inject {
         Self { lines }
     }
 
-    /// Returns the original content plus the newly injected conted within with updated indexes
-    pub fn inject_lines(&self, injection_index : usize, lines : &[String], consider_indentation : bool) -> Vec<(usize, String)> {
+    /// Returns the original content plus the newly injected content after the regarded injection index
+    pub fn inject_lines(
+        &self,
+        injection_index: usize,
+        lines: &[String],
+        consider_indentation: bool,
+    ) -> Vec<(usize, String)> {
+
         let original_lines = self.lines.clone();
         let mut new_lines: Vec<String> = Vec::new();
-        if injection_index == 0 { // in case the client wants to inject at the top of the file
-            lines.iter().for_each(|string| new_lines.push(string.to_owned()));
-            original_lines.iter().for_each(|(_, string)| new_lines.push(string.to_owned()));
-        } else if injection_index >= original_lines.len() { // in case the client wants to inject at the bottom of the file
-            original_lines.iter().for_each(|(_, string)| new_lines.push(string.to_owned()));
-            let last_line_indent_size = tab_size(&original_lines[original_lines.len() - 1].1);
+
+        if injection_index == 0 {
+            // Meaning the client wants to inject at the top of file
+            new_lines.extend(lines.to_vec());
+            original_lines.iter().for_each(|(_, line)| new_lines.push(line.to_owned()));
+        } else if injection_index >= original_lines.len() {
+            // Meaning the client wants to inject at the bottom of file
+            original_lines.iter().for_each(|(_, line)| new_lines.push(line.to_owned()));
             if consider_indentation {
-                lines.iter().for_each(|string| new_lines.push(indent_line(last_line_indent_size, string)));
+                let last_line_indentation_size = tab_size(&original_lines[original_lines.len() - 1].1);
+                lines.iter().for_each(|line| new_lines.push(indent_line(last_line_indentation_size, &line)));
             } else {
-                lines.iter().for_each(|string| new_lines.push(string.to_owned()));
+                new_lines.extend(lines.to_vec());
             }
         } else {
             for (index, line) in original_lines.iter() {
-                if *index + 1 == injection_index { // adding +1 for injection that makes more sense. EX. when specifying 1 as injection index, it SHOULD inject in the #1 indexed line
-                    if consider_indentation {
-                        let indentation_size = tab_size(&line);
-                        new_lines.push(indent_line(indentation_size, &line));
-                        continue;
-                    }
+                if *index == injection_index {
                     new_lines.push(line.to_owned());
+                    lines.iter().for_each(|_line| {
+                        if consider_indentation {
+                            let indentation_size = tab_size(&line);
+                            new_lines.push(indent_line(indentation_size, &_line));
+                        } else {
+                            new_lines.push(_line.to_owned());
+                        }
+                    });
                     continue;
                 }
                 new_lines.push(line.to_owned());
             }
         }
+
         let enumerated_lines: Vec<(usize, String)> = new_lines
             .iter()
             .enumerate()
