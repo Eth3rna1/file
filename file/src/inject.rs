ba9c6f57c85d2512816d636cfa252d6fdd4e5bad
Gabriel Mendieta Hernandez <131297265+Eth3rna1@users.noreply.github.com>
2024-05-09 12:41:07 -0500

    Update inject.rs
    Improved the `inject_lines` method to be more versatile

--- file/src/inject.rs
+++ file/src/inject.rs
@@ -4,6 +4,24 @@
 use crate::tool::{self, Constructor};
 use std::collections::HashMap;
 
+fn tab_size(line : &str) -> usize {
+    let mut size: usize = 0;
+    for character in line.chars() {
+        match character {
+            ' ' => size += 1,
+            '\t' => size += 4,
+            _ => break,
+        };
+    }
+    size
+}
+
+fn indent_line(indent_size : usize, line : &str) -> String {
+    let mut interface = " ".repeat(indent_size);
+    interface += line;
+    interface
+}
+
 pub struct Inject {
     lines: Vec<(usize, String)>,
 }
@@ -13,43 +31,41 @@ impl Inject {
         Self { lines }
     }
 
-    fn join_lines(&self, _lines: &[(usize, String)]) -> Vec<(usize, String)> {
-        let mut lines: HashMap<usize, String> = HashMap::from_iter(self.lines.clone());
-        let new_lines: HashMap<usize, String> = HashMap::from_iter(_lines.to_vec());
-        lines.extend(new_lines);
-        let max: usize = *lines.keys().max().unwrap();
-        let mut joined_lines: Vec<(usize, String)> = Vec::new();
-        for i in 0..=max {
-            joined_lines.push((i, lines[&i].to_owned()));
-        }
-        joined_lines
-    }
-
     /// Returns the original content plus the newly injected conted within with updated indexes
-    pub fn inject_lines(
-        &mut self,
-        index: usize,
-        lines: &[(usize, String)],
-        with_indentation: bool,
-    ) -> Result<Vec<(usize, String)>, &'static str> {
-        if self.lines.iter().all(|(_index, _)| *_index != index) {
-            // meaning index does not exist
-            return Err("Index out of bounds");
-        }
-        let mut original_lines = self.lines.clone();
-        let new_lines: Vec<(usize, String)>;
-        if with_indentation {
-            let indent_size =
-                tool::measure_indent_size(&original_lines[original_lines.len() - 1].1);
-            let mut modified_lines: Vec<(usize, String)> = Vec::new();
-            lines.into_iter().for_each(|(ind, stri)| {
-                modified_lines.push((*ind, format!("{}{}", " ".repeat(indent_size), stri)))
-            });
-            new_lines = self.join_lines(&modified_lines);
+    pub fn inject_lines(&self, injection_index : usize, lines : &[String], consider_indentation : bool) -> Vec<(usize, String)> {
+        let original_lines = self.lines.clone();
+        let mut new_lines: Vec<String> = Vec::new();
+        if injection_index == 0 { // in case the client wants to inject at the top of the file
+            lines.iter().for_each(|string| new_lines.push(string.to_owned()));
+            original_lines.iter().for_each(|(_, string)| new_lines.push(string.to_owned()));
+        } else if injection_index >= original_lines.len() { // in case the client wants to inject at the bottom of the file
+            original_lines.iter().for_each(|(_, string)| new_lines.push(string.to_owned()));
+            let last_line_indent_size = tab_size(&original_lines[original_lines.len() - 1].1);
+            if consider_indentation {
+                lines.iter().for_each(|string| new_lines.push(indent_line(last_line_indent_size, string)));
+            } else {
+                lines.iter().for_each(|string| new_lines.push(string.to_owned()));
+            }
         } else {
-            new_lines = self.join_lines(lines);
+            for (index, line) in original_lines.iter() {
+                if *index + 1 == injection_index { // adding +1 for injection that makes more sense. EX. when specifying 1 as injection index, it SHOULD inject in the #1 indexed line
+                    if consider_indentation {
+                        let indentation_size = tab_size(&line);
+                        new_lines.push(indent_line(indentation_size, &line));
+                        continue;
+                    }
+                    new_lines.push(line.to_owned());
+                    continue;
+                }
+                new_lines.push(line.to_owned());
+            }
         }
-        Ok(new_lines)
+        let enumerated_lines: Vec<(usize, String)> = new_lines
+            .iter()
+            .enumerate()
+            .map(|(index, string)| (index + 1, string.to_owned()))
+            .collect();
+        enumerated_lines
     }
 }
 
